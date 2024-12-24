1de4bcc05fa24b917564ccbc9ca6a63aa6e8e6c7
Fransiskus A Tekege <154669382+Fransis96@users.noreply.github.com>
2024-12-24 18:55:57 +0700

    Update and rename MarsPhoto.md to MarsPhoto.kt

--- Internet Image/MarsPhoto.kt
+++ Internet Image/MarsPhoto.kt
@@ -1,10 +1,8 @@
-## MarsPhoto.kt
-File type ``Data Class`` 
-```
+// MarsPhoto.kt
+// File type -> Data Class
 package com.example.internetimage
 
 data class MarsPhoto(
     val id: String,
     val img_src: String
 )
-```
